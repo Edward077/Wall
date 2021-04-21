41b92596b4161da235f335c5b6279574715e5c34
Edward David Thoronka <edward.thoronka.ubuntuafrika@gmail.com>
2021-04-21 01:48:58 +0000

    Wall Area Code

---
+++ src/com/Edward/Main.java
@@ -0,0 +1,14 @@
+package com.Edward;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// this is the main
+        Wall w = new Wall();
+        w.setHeight(5.7);
+        w.setWidth(2.3);
+        w.getArea();
+        System.out.println(w.getArea());
+
+    }
+}
